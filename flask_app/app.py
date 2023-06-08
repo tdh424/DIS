dfb1fd0056f9b6f06876556caa04f6ac808310d9
Ramsbaek <70701908+Ramsbaek@users.noreply.github.com>
2023-06-08 13:46:42 +0200

    done

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,34 +1,52 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+import yfinance as yf
+import plotly.graph_objects as go
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://@localhost/emil'
-db = SQLAlchemy(app)
-
-
-class Stock(db.Model):
-    __tablename__ = 'stocks'
-    datetime = db.Column(db.DateTime, primary_key=True, name="Datetime")
-    open = db.Column(db.Float, name="Open")
-    high = db.Column(db.Float, name="High")
-    low = db.Column(db.Float, name="Low")
-    close = db.Column(db.Float, name="Close")
-    adj_close = db.Column(db.Float, name="Adj_Close")
-    volume = db.Column(db.Integer, name="Volume")
-    ticker = db.Column(db.String(10), name="Ticker")
-
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
 @app.route('/stock_graph')
 def stock_graph():
-    stocks = Stock.query.all()
+    # Fetch data from yfinance
+    data = yf.download(tickers='AAPL', period='1d', interval='1m')
 
-    return render_template('stock_graph.html', stocks=stocks)
+    # Create the candlestick chart
+    fig = go.Figure(data=[go.Candlestick(
+        x=data.index,
+        open=data['Open'],
+        high=data['High'],
+        low=data['Low'],
+        close=data['Close'],
+        name='market data'
+    )])
 
+    # Add titles and axis labels
+    fig.update_layout(
+        title='AAPL Stock Price Evolution',
+        yaxis_title='Stock Price (USD per Share)'
+    )
+
+    # Customize x-axis range selector
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=15, label='15m', step='minute', stepmode='backward'),
+                dict(count=45, label='45m', step='minute', stepmode='backward'),
+                dict(count=1, label='HTD', step='hour', stepmode='todate'),
+                dict(count=3, label='3h', step='hour', stepmode='backward'),
+                dict(step='all')
+            ])
+        )
+    )
+
+    # Convert the figure to JSON for passing to the template
+    graph_json = fig.to_json()
+
+    return render_template('stock_graph.html', graph_json=graph_json)
 
 if __name__ == '__main__':
     app.run(debug=True)
