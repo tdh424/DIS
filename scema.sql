473a378d29b40284b9895f2e9ff9262e45b5a5d5
Ramsbaek <70701908+Ramsbaek@users.noreply.github.com>
2023-06-08 13:48:50 +0200

    Update scema.sql

--- scema.sql
+++ scema.sql
@@ -1,10 +1,13 @@
+\c stonkviewer
+DROP TABLE IF EXISTS stocks;
 CREATE TABLE stocks (
-    id SERIAL PRIMARY KEY,
-    datetime TIMESTAMP,
-    open FLOAT,
-    high FLOAT,
-    low FLOAT,
-    close FLOAT,
-    adj_close FLOAT,
-    volume INTEGER
+    "Datetime" timestamp with time zone,
+    "Open" double precision,
+    "High" double precision,
+    "Low" double precision,
+    "Close" double precision,
+    "Adj_Close" double precision,
+    "Volume" bigint,
+    "Ticker" varchar(10)
 );
+CREATE INDEX ix_stocks_Datetime ON stocks("Datetime");
